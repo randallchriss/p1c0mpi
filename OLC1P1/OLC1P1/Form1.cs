6359b475b0c2bc59c1033e1eb3da5d179050e6f1
Randall Ramos <randall.saucedo@gmail.com>
2020-03-06 15:36:07 -0600

    Adding tabs

--- OLC1P1/OLC1P1/Form1.cs
+++ OLC1P1/OLC1P1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,10 +17,40 @@ namespace OLC1P1
         {
             InitializeComponent();
         }
+        // variables globales
+        int contador = 0;
+        Boolean hayPestania = false;
+        int pActiva = 0;
+        public ArrayList tabsArray = new ArrayList();
+        public ArrayList txtArray = new ArrayList();
+        public ArrayList listaTitulos = new ArrayList();
+        public ArrayList rutas = new ArrayList();
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {// agregar nueva pestania en blanco
+            contador++;
+            int nombreTab = contador;
+            int pestaniaActiva = 0;
+            TextBox txtContenido = new TextBox();
+            txtContenido.Multiline = true;
+            txtContenido.SetBounds(0, 10, 500, 320);
+
+            TabPage t = new TabPage();
+            tabsArray.Add(t);
+            t.Controls.Add(txtContenido);
+            tabControl1.TabPages.Add(t);
+            pestaniaActiva = tabControl1.SelectedIndex;
+
+            hayPestania = true;
+            int cantidadTabs = tabsArray.Count;
+
+            
         }
     }
 }
